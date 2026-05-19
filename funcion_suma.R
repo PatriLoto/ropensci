f26e38a048a3396169342c8ee30fb8acc76e5512
PatriLoto <patricialoto@hotmail.com>
2026-05-19 20:19:47 -0300

    se agrega una variable

--- funcion_suma.R
+++ funcion_suma.R
@@ -1,5 +1,7 @@
-# mi funcion favorita
+# mi funcion favorita: recibe 3 variables numéricas, x e y, y devuelve 
+# la suma de los 3.
 
 suma <- function(x,y,z){
-   x + y + z
+   resultado <- x + y + z
+  return(resultado)
 }
